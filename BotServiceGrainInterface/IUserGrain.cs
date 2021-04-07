cec235caef6f9145d9d8b061b7ebf8540f89302f
Christian Corsano <christian.corsano@gmail.com>
2021-04-07 02:29:08 +0200

    Playing with Razor, getting fast to the same point as without

--- BotServiceGrainInterface/IUserGrain.cs
+++ BotServiceGrainInterface/IUserGrain.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BotServiceGrainInterface
+namespace BotServiceGrain
 {
     public interface IUserGrain : IGrainWithStringKey
     {
