bdc03c077dfbbfbb9eaaca691014e45ac426776b
Christian Corsano <christian.corsano@gmail.com>
2021-04-30 21:43:41 +0200

    Channel selection wip

---
+++ BlipBloopWeb/Model/AvailableChannel.cs
@@ -0,0 +1,15 @@
+﻿using BotServiceGrainInterface.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlipBloopWeb.Model
+{
+    public class AvailableChannel
+    {
+        public string BroadcasterId { get; set; }
+        public string BroadcasterName { get; set; }
+        public ChannelRole UserRole { get; set; }
+    }
+}
