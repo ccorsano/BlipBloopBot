bb1ab82e56853d91eb7150c284ef8cb83bceefa1
Christian Corsano <christian.corsano@gmail.com>
2021-05-14 12:49:27 +0200

    Functional synopsis page

---
+++ Conceptoire.Twitch/API/HelixGamesResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Conceptoire.Twitch.API
+{
+    public class HelixGamesResponse
+    {
+        [JsonPropertyName("data")]
+        public HelixCategoriesSearchEntry[] Data { get; set; }
+    }
+}
