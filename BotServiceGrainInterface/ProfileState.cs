f1304f9f9872748ebf84fcb4f02a8bf704dbd5a0
Christian Corsano <christian.corsano@gmail.com>
2021-05-28 21:40:45 +0200

    Don't request chat write access unless needed

--- BotServiceGrainInterface/ProfileState.cs
+++ BotServiceGrainInterface/ProfileState.cs
@@ -1,5 +1,6 @@
 ﻿using BotServiceGrainInterface.Model;
 using Conceptoire.Twitch.API;
+using Conceptoire.Twitch.Constants;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,7 @@ namespace BotServiceGrainInterface
     {
         public bool HasActiveChannel { get; set; } = false;
         public string OAuthToken { get; set; }
+        public TwitchConstants.TwitchOAuthScopes[] CurrentScopes { get; set; } = new TwitchConstants.TwitchOAuthScopes[0];
         public List<HelixChannelInfo> CanBeBotOnChannels { get; set; } = new List<HelixChannelInfo>();
         public HashSet<UserRole> Roles { get; set; } = new HashSet<UserRole>();
     }
